dba3d884d9b1b6f6bdeb80eeed04e11cfc4080ee
anntogasaki <67474211+anntogasaki@users.noreply.github.com>
2020-07-29 08:30:54 -0700

    Update variables.tf
    changing parameters back. (uncommenting)

--- variables.tf
+++ variables.tf
@@ -3,43 +3,43 @@ variable "TF_VERSION" {
  description = "terraform engine version to be used in schematics"
 }
 
-#variable "ibmcloud_api_key" {
-#   defined in tfvars
-#    description = "The user's IBM Cloud api key."
-#}
+variable "ibmcloud_api_key" {
+   defined in tfvars
+    description = "The user's IBM Cloud api key."
+}
 
-#variable "ssh_key" {
-#   defined in tfvars
-#  description = "The name of the public SSH key to be used when provisining Delphix VSI."
-#}
+variable "ssh_key" {
+   defined in tfvars
+  description = "The name of the public SSH key to be used when provisining Delphix VSI."
+}
 
-#variable "iaas_classic_username" {
-#  defined in tfvars
-#   description = "The user's classic infastructure user name."
-#}
+variable "iaas_classic_username" {
+  defined in tfvars
+   description = "The user's classic infastructure user name."
+}
 
-#variable "iaas_classic_api_key" {
-#   defined in tfvars
-#    description = "The account's classic infastructure api key."
-#}
+variable "iaas_classic_api_key" {
+   defined in tfvars
+    description = "The account's classic infastructure api key."
+}
 
-#variable "hostname" {
-#   default region to be used
-#    default = "ddpeng1"
-#    description = "The name of your Delphix Virtual Server to be provisioned."
-#}
+variable "hostname" {
+   default region to be used
+    default = "ddpeng1"
+    description = "The name of your Delphix Virtual Server to be provisioned."
+}
 
-#variable "region" {
-#   default region to be used
-#    default = "us-east"
-#    description = "Optional. The value of the region of VPC."
-#}
+variable "region" {
+   default region to be used
+    default = "us-east"
+    description = "Optional. The value of the region of VPC."
+}
 
-#variable "profile" {
-#   default compute profile to be used
-#    default = "mx2-16x128"
-#    description = "The profile of compute CPU and memory resources to be used when provisioning the Delphix VSI. For a list of available profiles, visit https://cloud.ibm.com/docs/vpc?topic=vpc-profiles"
-#}
+variable "profile" {
+   default compute profile to be used
+    default = "mx2-16x128"
+    description = "The profile of compute CPU and memory resources to be used when provisioning the Delphix VSI. For a list of available profiles, visit https://cloud.ibm.com/docs/vpc?topic=vpc-profiles"
+}
 
 variable "volumecount" {
   # default volume count
@@ -59,15 +59,16 @@ variable "volumesize" {
     description = "The block storage volume size in GB. Enter a size between 10 and 2000 GB."
 }
 
-#variable "vpcname" {
-#   default vpc name to be used
-#  default = "delphix-test-vpc"
-#  description = "The name of your VPC where the Delphix  VSI is to be provisioned."
-#}
-#variable "zone" {
-#  default     = "us-south"
-#  description = "The VPC Zone that you want your VPC networks and virtual servers to be provisioned in. To list available zones, run `ibmcloud is zones`."
-#}
+variable "vpcname" {
+   default vpc name to be used
+  default = "delphix-test-vpc"
+  description = "The name of your VPC where the Delphix  VSI is to be provisioned."
+}
+
+variable "zone" {
+  default     = "us-south"
+  description = "The VPC Zone that you want your VPC networks and virtual servers to be provisioned in. To list available zones, run `ibmcloud is zones`."
+}
 
 variable "resource_group" {
   # use default or whatever valid resource name in your account
@@ -85,7 +86,7 @@ variable "vnf_vpc_image_name" {
   description = "The name of the custom image to be provisioned in your IBM Cloud account."
 }
 
-#variable "subnet_id" {
-#  default = "dev-sub1"
-#  description = "The id of the subnet where the Delhpix VSI is to be provisioned."
-#}
+variable "subnet_id" {
+  default = "dev-sub1"
+  description = "The id of the subnet where the Delhpix VSI is to be provisioned."
+}
