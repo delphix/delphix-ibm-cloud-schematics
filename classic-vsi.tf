1dfdd27aa7c83f47c72ee58cef5bb97f20f4cee3
anntogasaki <67474211+anntogasaki@users.noreply.github.com>
2020-09-15 14:26:43 -0700

    Update classic-vsi.tf

--- classic-vsi.tf
+++ classic-vsi.tf
@@ -57,7 +57,7 @@ resource "ibm_is_instance" "instance" {
   vpc       = "${data.ibm_is_vpc.myvpc.id}"
   zone    = "${var.zone}"
   keys      = ["${data.ibm_is_ssh_key.ssh_key_id.id}"]
-  volumes = ["${ibm_is_volume.new_volume.*.id}"]
+  volumes = "${ibm_is_volume.new_volume.*.id}"
   #volumes = ["${element((ibm_is_volume.new_volume.*.id),0)}",
   #          "${element((ibm_is_volume.new_volume.*.id), 1)}",
   #          "${element((ibm_is_volume.new_volume.*.id), 2)}",
